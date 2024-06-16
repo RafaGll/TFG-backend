6357f29071f6e3bcde6ba83d8b4a22313fddbcea
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2024-06-16 18:47:06 +0200

    eliminadas ciertas lineas para evitar errores

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-require("dotenv").config(); // Asegúrate de cargar dotenv al inicio
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors({ origin: "http://localhost:3001" }));
+app.use(cors());
 app.use(express.json());
 
 const tutorialsRouter = require("./routes/tutorials");
@@ -17,15 +17,8 @@ app.use("/tutorials", tutorialsRouter);
 app.use("/exercises", exercisesRouter);
 app.use("/auth", authRouter);
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(port, () => {
