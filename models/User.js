2e2c73565fcd531f87beba6e65138ba117a7ca6a
Rafael González <rafaelgonzalezllamas2001@gmail.com>
2024-07-06 05:22:17 +0200

    update

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 
 const CompletedExerciseSchema = new mongoose.Schema({
   category: { type: mongoose.Schema.Types.ObjectId, ref: "Category", required: true },
